05193cf7de130535dca5530de36b0aa67b074762
ThomBurt <93936296+ThomBurt@users.noreply.github.com>
2022-02-25 14:18:20 -0500

    routes written out

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -27,18 +27,79 @@ router.get("/", async (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: {
+      model: Product,
+      attributes: ["category_id"],
+    },
+  })
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  .then((categoryData) => res.json(categoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name,
+  })
+    .then((categoryData) => res.json(categoryData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    })
+    .then((categoryData) => {
+      if (!categoryData) {
+        res.status(404).json({ message: "Category NOT Found with this ID" });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+    
+  })
+    .then((categoryData) => {
+      if (!categoryData) {
+        res.status(404).json({ message: "Category NOT Found with Given ID" });
+        return;
+      }
+      res.json(categoryData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
